f149b028ba106b77b931ec4dbd4a8f03a1de1c7e
just1 <justin.jiang@duke.edu>
2024-02-27 01:12:05 -0500

    re-organized

--- turbo_debug.py
+++ turbo_debug.py
@@ -1,10 +1,12 @@
 import os
 import argparse
 import torch
+import time
 import tomesd
+from collections import defaultdict
 from torch.cuda.amp import autocast
-from torch.profiler import profile, record_function, ProfilerActivity
 from scripts.demo.streamlit_helpers import *
+from prompting import PROMPT
 
 #####################################################################################
 # helper functions from original turbo demo
@@ -113,7 +115,9 @@ def sample(
         seed=0,
         filter=None,
 ):
-    """sampling function with torch profiling"""
+    """
+    sampling function with torch profiling
+    """
     F = 8
     C = 4
     shape = (1, C, H // F, W // F)
@@ -134,48 +138,59 @@ def sample(
     precision_scope = autocast
     with torch.no_grad():
         with precision_scope("cuda"):
-            # start profile
-            with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
-                         record_shapes=True,
-                         profile_memory=True,
-                         with_stack=True) as prof:
-                with record_function("model_inference"):
-                    batch, batch_uc = get_batch(
-                        get_unique_embedder_keys_from_conditioner(model.conditioner),
-                        value_dict,
-                        [1],
-                    )
-                    c = model.conditioner(batch)
-                    uc = None
-                    randn = seeded_randn(shape, seed)
+            batch, batch_uc = get_batch(
+                get_unique_embedder_keys_from_conditioner(model.conditioner),
+                value_dict,
+                [1],
+            )
+            c = model.conditioner(batch)
+            uc = None
+            randn = seeded_randn(shape, seed)
 
-                    def denoiser(input, sigma, c):
-                        return model.denoiser(
-                            model.model,
-                            input,
-                            sigma,
-                            c,
-                        )
+            def denoiser(input, sigma, c):
+                return model.denoiser(
+                    model.model,
+                    input,
+                    sigma,
+                    c,
+                )
 
-                    samples_z = sampler(denoiser, randn, cond=c, uc=uc)
-                    samples_x = model.decode_first_stage(samples_z)
-                    samples = torch.clamp((samples_x + 1.0) / 2.0, min=0.0, max=1.0)
-                    if filter is not None:
-                        samples = filter(samples)
-                    samples = (
-                        (255 * samples)
-                        .to(dtype=torch.uint8)
-                        .permute(0, 2, 3, 1)
-                        .detach()
-                        .cpu()
-                        .numpy()
-                    )
-            print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
+            samples_z = sampler(denoiser, randn, cond=c, uc=uc)
+            samples_x = model.decode_first_stage(samples_z)
+            samples = torch.clamp((samples_x + 1.0) / 2.0, min=0.0, max=1.0)
+            if filter is not None:
+                samples = filter(samples)
+            samples = (
+                (255 * samples)
+                .to(dtype=torch.uint8)
+                .permute(0, 2, 3, 1)
+                .detach()
+                .cpu()
+                .numpy()
+            )
     return samples
 
 
+def profile_sampling(model, sampler, seed, prompt, iterations, filter=None):
+    """
+    profile multiple sampling processes
+    """
+    total_samples = []
+    start = time.time()
+
+    for i in range(iterations):
+        single_prompt = prompt[i]
+        samples = sample(model, sampler, H=512, W=512, seed=seed, prompt=single_prompt, filter=filter)
+        total_samples.append(samples)
+    total_runtime = (time.time() - start) / iterations
+
+    return total_samples, total_runtime
+
+
 def load_model_from_config(config, ckpt=None, verbose=True):
-    """an implementation without Streamlit"""
+    """
+    an implementation without Streamlit
+    """
     model = instantiate_from_config(config.model)
     if ckpt is not None:
         print(f"Loading model from {ckpt}")
@@ -209,7 +224,7 @@ def load_model_from_config(config, ckpt=None, verbose=True):
     return model, msg
 
 
-def init_without_st(version_dict, load_ckpt=True, load_filter=True, token_merging=True):
+def init_without_st(version_dict, load_ckpt=True, load_filter=True, tome_ratio=0.5):
     state = dict()
     if not "model" in state:
         config = version_dict["config"]
@@ -218,9 +233,8 @@ def init_without_st(version_dict, load_ckpt=True, load_filter=True, token_mergin
         config = OmegaConf.load(config)
         model, msg = load_model_from_config(config, ckpt if load_ckpt else None)
 
-        #TODO Token merging doesn't really work here
-        if token_merging:
-            tomesd.apply_patch(model, ratio=0.5)
+        if tome_ratio > 0:
+            model = tomesd.apply_patch(model, ratio=tome_ratio)
 
         state["msg"] = msg
         state["model"] = model
@@ -231,47 +245,82 @@ def init_without_st(version_dict, load_ckpt=True, load_filter=True, token_mergin
     return state
 
 
+def init_and_sampling(version_dict, sampler, seed, prompt, iterations, tome_ratio=0.5):
+    print(f"Initializing and sampling with TOME ratio = {tome_ratio} ...")
+    state = init_without_st(version_dict, load_ckpt=True, load_filter=True, tome_ratio=tome_ratio)
+    model = state["model"]
+    load_model(model)
+    samples, total_runtime = profile_sampling(model, sampler, seed, prompt, iterations, filter=state.get("filter"))
+    return samples, total_runtime
+
+
+def images_to_grid(images, grid_size=None, save_path="output_grid.png"):
+    if grid_size is None:
+        grid_cols = int(math.ceil(math.sqrt(len(images))))
+        grid_rows = int(math.ceil(len(images) / grid_cols))
+    else:
+        grid_rows, grid_cols = grid_size
+
+    # Assuming all images are the same size and removing the batch dimension
+    img_height, img_width = images[0].shape[1:3]
+    grid_img = Image.new('RGB', size=(img_width * grid_cols, img_height * grid_rows))
+
+    for index, img in enumerate(images):
+        row = index // grid_cols
+        col = index % grid_cols
+        # Reshape or select the slice to remove the batch dimension
+        img_pil = Image.fromarray(img[0])  # This selects the first (and only) image in the batch
+        grid_img.paste(img_pil, box=(col * img_width, row * img_height))
+
+    grid_img.save(save_path)
+    # grid_img.show()  # Optionally display the grid
+
+def save_samples_in_grids(samples):
+    for tome_ratio, images_list in samples.items():
+        # Flatten the list of image information to a list of numpy arrays
+        images = [img_info for img_info in images_list]
+        save_path = f"output_images_grid_tome_{tome_ratio}.png"
+        images_to_grid(images, save_path=save_path)
+        print(f"Saved images grid for ToMe ratio {tome_ratio} at: {save_path}")
+
 def main():
     parser = argparse.ArgumentParser(description="Generate images with Stable Diffusion Turbo.")
     parser.add_argument("--version", choices=VERSION2SPECS.keys(), default="SDXL-Turbo", help="Model version to use.")
     parser.add_argument("--n_steps", type=int, default=4, help="Number of sampling steps.")
-    parser.add_argument("--seed", type=int, default=42, help="Seed for random number generation.")
-    parser.add_argument("--prompt",
-                        default="A cinematic shot of a baby racoon wearing an intricate Italian priest robe.",
-                        help="Prompt for the model.")
+    parser.add_argument("--seed", type=int, default=0, help="Seed for random number generation.")
     parser.add_argument("--output", default="output_image.png", help="Output image path.")
-    parser.add_argument("--tome", action=argparse.BooleanOptionalAction, help="Token merging")
     args = parser.parse_args()
 
     version_dict = VERSION2SPECS[args.version]
-    if args.tome:
-        print("Initializing the state WITH Token Merging")
-    else:
-        print("Initializing the state WITHOUT Token Merging")
-    state = init_without_st(version_dict, load_ckpt=True, load_filter=True, token_merging=args.tome)
-
-    if state["msg"]:
-        print(state["msg"])
-    model = state["model"]
-    load_model(model)
-
-    sampler = SubstepSampler(
-        n_sample_steps=args.n_steps,
-        num_steps=1000,
-        eta=1.0,
-        discretization_config=dict(
-            target="sgm.modules.diffusionmodules.discretizer.LegacyDDPMDiscretization"
-        ),
-    )
-
+    sampler = SubstepSampler(n_sample_steps=args.n_steps, num_steps=1000, eta=1.0, discretization_config=dict(
+        target="sgm.modules.diffusionmodules.discretizer.LegacyDDPMDiscretization"))
     sampler.noise_sampler = SeededNoise(seed=args.seed)
-    out = sample(
-        model, sampler, H=512, W=512, seed=args.seed, prompt=args.prompt, filter=state.get("filter")
-    )
+    prompts = PROMPT
 
-    Image.fromarray(out[0]).save(args.output)
-    print(f"Image saved to {args.output}")
+    def ddict():
+        return defaultdict(ddict)
+    runtimes = ddict()
+    samples = ddict()
 
+    for tome_ratio in [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6]:
+        print(f"\tToMe ratio: {tome_ratio}")
+        samples[tome_ratio], runtimes[tome_ratio] = init_and_sampling(
+             version_dict, sampler, args.seed, prompts, len(prompts), tome_ratio=tome_ratio
+        )
+        print(f"\t\tRuntime: {runtimes[tome_ratio]:.3f}")
+
+    ## Print Results
+    for tome_ratio, runtime in runtimes.items():
+        no_tome_runtime = runtimes[0]  # Assuming '0' is the key for the base case without ToMe.
+        time_perc = 100 * (no_tome_runtime - runtime) / no_tome_runtime
+        print(
+            f"ToMe ratio: {tome_ratio:.1f} -- runtime reduction: {time_perc:5.2f}%")
+
+    save_samples_in_grids(samples)
+
+def diffuser_main():
+    # TODO: a alternate main method with diffusers
+    ...
 
 if __name__ == "__main__":
     main()
